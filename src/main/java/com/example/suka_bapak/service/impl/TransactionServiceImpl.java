51011005212793fc1f747daa348235734fbc8edc
Reynard A. <reynardaurele1510@gmail.com>
2024-09-11 11:31:45 +0700

    Transaction Repo Serv

--- src/main/java/com/example/suka_bapak/service/impl/TransactionServiceImpl.java
+++ src/main/java/com/example/suka_bapak/service/impl/TransactionServiceImpl.java
@@ -1,5 +1,15 @@
 package com.example.suka_bapak.service.impl;
 
 
-public class TransactionServiceImpl {
+import com.example.suka_bapak.repository.TransactionRepository;
+import com.example.suka_bapak.service.TransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TransactionServiceImpl implements TransactionService {
+    @Autowired
+    private TransactionRepository transactionRepository;
+
+
 }
