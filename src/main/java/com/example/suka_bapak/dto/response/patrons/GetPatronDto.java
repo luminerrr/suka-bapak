9a80bf3956852b8f81a80262ecd7a375fa7ac278
Kevin Ezra <tkevinrocket@gmail.com>
2024-09-11 14:51:21 +0700

    add annotator data and args constructor

--- src/main/java/com/example/suka_bapak/dto/response/patrons/GetPatronDto.java
+++ src/main/java/com/example/suka_bapak/dto/response/patrons/GetPatronDto.java
@@ -5,8 +5,12 @@ import java.time.LocalDate;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
+@NoArgsConstructor
+@Data
 
 public class GetPatronDto {
     @JsonProperty("id")
