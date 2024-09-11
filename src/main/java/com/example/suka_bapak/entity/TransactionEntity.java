3885c120520802cd6af61ec74d802b17162628f7
Kevin Ezra <tkevinrocket@gmail.com>
2024-09-11 10:31:36 +0700

    create transactionentity

---
+++ src/main/java/com/example/suka_bapak/entity/TransactionEntity.java
@@ -0,0 +1,49 @@
+package com.example.suka_bapak.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "transactions")
+@Data
+
+public class TransactionEntity implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // patron id
+    @OneToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "patron_id", referencedColumnName = "id")
+    private PatronEntity patron;
+
+    // book_id
+    @OneToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "book_id", referencedColumnName = "id")
+    private BookEntity book;
+
+    // borrow_date
+    @Column(name = "borrow_date")
+    @JsonProperty("borrow_date")
+    private Date borrowDate;
+    
+    // due date
+    @Column(name = "due_date")
+    @JsonProperty("due_date")
+    private Date dueDate;
+    
+    // return date
+    @Column(name = "return_date")
+    @JsonProperty("return_date")
+    private Date returnDate;
+
+    // fine
+    @Column(name = "fine")
+    @JsonProperty("fine")
+    private Float fine;
+}
