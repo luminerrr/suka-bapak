acbbcc68789c7e3e48fac17694c132287a817f64
Kevin Ezra <tkevinrocket@gmail.com>
2024-09-11 09:55:04 +0700

    init ceunah

---
+++ src/main/java/com/example/suka_bapak/SukaBapakApplication.java
@@ -0,0 +1,13 @@
+package com.example.suka_bapak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SukaBapakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SukaBapakApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/suka_bapak/SukaBapakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.suka_bapak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SukaBapakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
